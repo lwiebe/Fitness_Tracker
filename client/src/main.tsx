f53a448e51fa7f47f8676f8669cc749f2a674a1e
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2026-02-07 13:15:03 +0100

    first commit

---
+++ client/src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext.tsx'
+import { AppProvider } from './context/AppContext.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <ThemeProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </ThemeProvider>
+  </BrowserRouter>,
+)
